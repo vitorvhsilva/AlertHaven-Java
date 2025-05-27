bf197a4b1e5d89770ca3dc4b9a7fc8103cf6eac1
joaogananca26 <joaogananca26@gmail.com>
2025-05-27 20:48:44 -0300

    Criando as classes exceptions

---
+++ AlertHaven/src/main/java/br/com/AlertHaven/AlertHaven/exception/AbrigoNaoEncontradoException.java
@@ -0,0 +1,7 @@
+package br.com.AlertHaven.AlertHaven.exception;
+
+public class AbrigoNaoEncontradoException extends RuntimeException {
+    public AbrigoNaoEncontradoException(String message) {
+        super(message);
+    }
+}
---
+++ AlertHaven/src/main/java/br/com/AlertHaven/AlertHaven/exception/UsuarioNaoEncontradoException.java
@@ -0,0 +1,7 @@
+package br.com.AlertHaven.AlertHaven.exception;
+
+public class UsuarioNaoEncontradoException extends RuntimeException{
+    public UsuarioNaoEncontradoException(String message) {
+        super(message);
+    }
+}
