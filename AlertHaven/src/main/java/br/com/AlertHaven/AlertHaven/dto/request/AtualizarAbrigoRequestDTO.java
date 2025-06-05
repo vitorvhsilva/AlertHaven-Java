3a5f1a9eef3aae6c197f74a0402511cd5aadc12a
joaogananca26 <joaogananca26@gmail.com>
2025-06-04 21:36:36 -0300

    Arrumando notações

--- AlertHaven/src/main/java/br/com/AlertHaven/AlertHaven/dto/request/AtualizarAbrigoRequestDTO.java
+++ AlertHaven/src/main/java/br/com/AlertHaven/AlertHaven/dto/request/AtualizarAbrigoRequestDTO.java
@@ -1,10 +1,17 @@
 package br.com.AlertHaven.AlertHaven.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class AtualizarAbrigoRequestDTO {
+    @NotBlank(message = "O nome do abrigo não pode ficar em branco")
     private String nomeAbrigo;
+
+    @NotBlank(message = "O email do abrigo não pode ficar em branco")
     private String emailAbrigo;
+
+    @NotNull
     private int capacidadeSuportadaAbrigo;
 }
--- AlertHaven/src/main/java/br/com/AlertHaven/AlertHaven/dto/response/AtualizarAbrigoResponseDTO.java
+++ AlertHaven/src/main/java/br/com/AlertHaven/AlertHaven/dto/response/AtualizarAbrigoResponseDTO.java
@@ -5,13 +5,8 @@ import lombok.Data;
 
 @Data
 public class AtualizarAbrigoResponseDTO {
-    @NotBlank(message = "O nome do abrigo não pode ficar em branco")
     private String nomeAbrigo;
-
-    @NotBlank(message = "O email do abrigo não pode ficar em brannco")
     private String emailAbrigo;
-
-    @NotBlank(message = "A capacidade do abrigo não pode ficar em branco")
     private int capacidadeSuportadaAbrigo;
 
 }
