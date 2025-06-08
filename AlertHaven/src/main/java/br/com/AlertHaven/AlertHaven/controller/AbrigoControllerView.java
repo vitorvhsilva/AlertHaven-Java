ed285adf623a77657558f659e970096efdb37f4e
vitorvhsilva <dndragonbr@gmail.com>
2025-06-08 01:35:49 -0300

    Cadastro de abrigos via MVC

--- AlertHaven/src/main/java/br/com/AlertHaven/AlertHaven/controller/AbrigoControllerView.java
+++ AlertHaven/src/main/java/br/com/AlertHaven/AlertHaven/controller/AbrigoControllerView.java
@@ -1,10 +1,17 @@
 package br.com.AlertHaven.AlertHaven.controller;
 
+import br.com.AlertHaven.AlertHaven.dto.request.CadastrarAbrigoRequestDTO;
 import br.com.AlertHaven.AlertHaven.entity.Abrigo;
 import br.com.AlertHaven.AlertHaven.repository.AbrigoRepository;
+import br.com.AlertHaven.AlertHaven.repository.TipoEmergenciaRepository;
+import br.com.AlertHaven.AlertHaven.service.AbrigoService;
+import br.com.AlertHaven.AlertHaven.service.LocalizacaoService;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
@@ -12,39 +19,47 @@ import org.springframework.web.bind.annotation.*;
 @AllArgsConstructor
 public class AbrigoControllerView {
 
-    private AbrigoRepository repository;
+    private AbrigoService abrigoService;
+    private LocalizacaoService localizacaoService;
+    private TipoEmergenciaRepository tipoEmergenciaRepository;
+    private ModelMapper mapper;
 
     @GetMapping("/listar")
     public String listarAbrigos(Model model) {
-        model.addAttribute("abrigos", repository.findAll());
+        model.addAttribute("abrigos", abrigoService.listarAbrigos());
         return "listarAbrigos";
     }
 
     @GetMapping("/adicionar")
-    public String formularioAbrigo(Model model) {
-        model.addAttribute("abrigo", new Abrigo());
+    public String mostrarFormulario(Model model) {
+        model.addAttribute("cadastrarAbrigoRequestDTO", new CadastrarAbrigoRequestDTO());
+        model.addAttribute("tiposEmergencia", tipoEmergenciaRepository.findAll());
         return "formularioAbrigo";
     }
 
     @PostMapping("/salvar")
-    public String salvarAbrigo(@ModelAttribute Abrigo abrigo) {
-        repository.save(abrigo);
-        return "redirect:/listarAbrigos";
+    public String salvarAbrigo(@Valid @ModelAttribute CadastrarAbrigoRequestDTO dto,
+                               BindingResult result,
+                               Model model) {
+        if (result.hasErrors()) {
+            model.addAttribute("tiposEmergencia", tipoEmergenciaRepository.findAll());
+            return "formularioAbrigo";
+        }
+        var abrigoCadastrado = abrigoService.cadastrarAbrigo(mapper.map(dto, Abrigo.class), dto.getIdsTipoEmergencia());
+        localizacaoService.persistirLocalizacao(dto.getCep(), abrigoCadastrado);
+        return "redirect:/abrigosview/listar";
     }
 
     @GetMapping("/editar/{id}")
     public String editarAbrigo(@PathVariable String id, Model model) {
-        Abrigo abrigo = repository.findById(id)
-                .orElseThrow(() -> new IllegalArgumentException("ID Inválido: " + id));
+        Abrigo abrigo = abrigoService.obterAbrigoPorId(id);
         model.addAttribute("abrigo", abrigo);
         return "formularioAbrigo";
     }
 
     @GetMapping("/excluir/{id}")
     public String excluirAbrigo(@PathVariable String id) {
-        Abrigo abrigo = repository.findById(id)
-                .orElseThrow(() -> new IllegalArgumentException("ID Inválido: " + id));
-        repository.delete(abrigo);
+        abrigoService.deletarAbrigoPorId(id);
         return "redirect:/listarAbrigos";
     }
 }
